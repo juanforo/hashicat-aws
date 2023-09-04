e729d846736357c05981a5682c7733668c878a8f
Julián Forero <juanforo94@hotmail.com>
2023-09-04 15:39:33 +0000

    Removing bucket creation due to permission's error

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "jforeror-aws"
     workspaces {
       name = "hashicat-aws-dev"
